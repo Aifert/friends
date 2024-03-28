1f03950bd99312286d6824168fd3d291bde2ef14
Aifert Yet <aifertyet0628@gmail.com>
2024-03-28 11:24:38 +0800

    finished implementing devise, working on styling devise now

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :friends
   # get 'home/index'
   get 'home/about'
